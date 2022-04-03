7f1c189aa958963d0a70bf7e334c0acbebd19403
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-02 22:47:12 -0400

    Created a game-manager to control the game's buttons and menu text. Plan to add a button method for moving from main scene to the menu scene.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    // PROPS - GATHER ON AWAKE
+    DataManager DM;
+
+    // PROPS - SERIALIZED
+    [SerializeField] private TMP_InputField textInput_playerName;
+    [SerializeField] private TMP_Text textDisplay_highScore;
+
+    // PROPS - MANUALLY SET
+    private string defaultPlayerName = "Your Name Here...";
+
+    private void Awake()
+    {
+        DM = GameObject.FindObjectOfType<DataManager>();
+    }
+
+    private void Start()
+    {
+        DM.LoadData();
+        textInput_playerName.SetTextWithoutNotify(DataManager.Instance.playerName);
+        textDisplay_highScore.text = "High Score: " + DataManager.Instance.highestScore + " : " + DataManager.Instance.playerName;
+    }
+
+    // MENU - START BUTTON
+    public void menubutton_start()
+    {
+        // HANDLE PLAYER NAME
+        string playerNameText = textInput_playerName.text;  // get input text
+        Debug.Log(playerNameText);                                  // log for reference
+
+        if (playerNameText.Equals(defaultPlayerName)) { // handle player name not set
+            playerNameText = "unknown";
+        }
+
+        DataManager.Instance.playerName = playerNameText; // persist scene transition
+        DM.SaveData(playerNameText);                                  // persist game session
+
+        // LOAD SCENE
+        SceneNavigator.GoToMainGameScene();
+    }
+}
