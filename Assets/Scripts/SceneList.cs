b8bae423440e62b19b5fb5ce2451a2b6291a6fa7
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-02 22:43:47 -0400

    Created and populated an Enum for Scenes

---
+++ Assets/Scripts/SceneList.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Scenes in the game.
+/// Enum is a zero-based list.
+/// </summary>
+public enum Scenes
+{
+    menu,
+    main
+}
